156ad5dbb1b77fd3fed793837920a229a729ef68
William Hilton <wmhilton@gmail.com>
2021-07-31 01:55:54 -0400

    fix paths

--- osx/main.js
+++ osx/main.js
@@ -1,4 +1,5 @@
 const { spawn } = require('child_process');
+const path = require('path');
 
 const stream = spawn('log', ['stream'])
 
@@ -6,10 +7,10 @@ stream.stdout.on('data', (data) => {
   const line = data.toString('utf8');
   if (line.includes('Post event kCameraStreamStart')) {
     console.log("Camera start");
-    spawn('node', ['ble.js', 'on,255,0,0,0.1'], { stdio: 'inherit' })
+    spawn('node', [path.join(__dirname, 'ble.js'), 'on,255,0,0,0.1'], { stdio: 'inherit' })
   }
   if (line.includes('Post event kCameraStreamStop')) {
     console.log("Camera stop");
-    spawn('node', ['ble.js', 'off'], { stdio: 'inherit' })
+    spawn('node', [path.join(__dirname, 'ble.js'), 'off'], { stdio: 'inherit' })
   }
 });
